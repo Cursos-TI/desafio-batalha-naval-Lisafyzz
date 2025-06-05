713b7a1adf6e8c7f3fb847459d8ba07ed42ac68a
Daoifyz <bfsfernandes126@gmail.com>
2025-06-05 07:22:37 +0000

    Nivel novato atualizado

--- Trabalho.c
+++ Trabalho.c
@@ -3,15 +3,15 @@
 int main() {
     int i, j;
 
-    // Criação do tabuleiro 10x10 com água (0)
+    // Criação do tabuleiro 10x10 com água 0
     int tabuleiro[10][10] = {0};
 
-    // Colocando navio horizontal (linha 2, colunas 4 a 6)
+    // Colocando navio horizontal linha 2, colunas 4 a 6
     tabuleiro[2][4] = 3;
     tabuleiro[2][5] = 3;
     tabuleiro[2][6] = 3;
 
-    // Colocando navio vertical (coluna 7, linhas 5 a 7)
+    // Colocando navio vertical coluna 7, linhas 5 a 7
     tabuleiro[5][7] = 3;
     tabuleiro[6][7] = 3;
     tabuleiro[7][7] = 3;
