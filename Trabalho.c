8a0089a0a6617cef5444037531a5a3d24fe7dbf9
Daoifyz <bfsfernandes126@gmail.com>
2025-06-05 10:04:03 +0000

    Nivel novato pronto

--- Trabalho.c
+++ Trabalho.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int i, j;
-
-    // Criação do tabuleiro 10x10 com água 0
     int tabuleiro[10][10] = {0};
+    int navioH[3]; // navio horizontal
+    int navioV[3]; // navio vertical
 
-    // Colocando navio horizontal linha 2, colunas 4 a 6
-    tabuleiro[2][4] = 3;
-    tabuleiro[2][5] = 3;
-    tabuleiro[2][6] = 3;
+    // Coordenadas iniciais
+    int linhaH = 2, colunaH = 4;
+    int linhaV = 5, colunaV = 6;
 
-    // Colocando navio vertical coluna 7, linhas 5 a 7
-    tabuleiro[5][7] = 3;
-    tabuleiro[6][7] = 3;
-    tabuleiro[7][7] = 3;
+    // Preenche navio horizontal
+    for (int i = 0; i < 3; i++) {
+        navioH[i] = colunaH + i;
+        tabuleiro[linhaH][colunaH + i] = 3;
+    }
 
-    // Exibindo o tabuleiro
-    printf("Tabuleiro de Batalha Naval:\n\n");
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+    // Preenche navio vertical
+    for (int i = 0; i < 3; i++) {
+        navioV[i] = linhaV + i;
+        tabuleiro[linhaV + i][colunaV] = 3;
+    }
+
+    // Exibe o tabuleiro
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
